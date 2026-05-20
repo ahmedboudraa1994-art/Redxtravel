4ddb95489226c9251ed6077d1f810477ab981411
ahmedboudraa1994-art <ahmed.boudraa1994@gmail.com>
2026-05-19 22:08:58 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
-window.REDX_FIREBASE_CONFIG={apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""};window.REDX_ADMIN={email:"admin@redxtravel.com",password:"redx2026"};
+window.REDX_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA_egIs64Hvsz-NTiNBSUv6SvG_t-Y9Ccw",
+  authDomain: "redx-travel-22181.firebaseapp.com",
+  projectId: "redx-travel-22181",
+  storageBucket: "redx-travel-22181.firebasestorage.app",
+  messagingSenderId: "752734940388",
+  appId: "1:752734940388:web:5f69dbcbbd207126d350f1"
+};
+
+window.REDX_ADMIN = {
+  email: "admin@redxtravel.com",
+  password: "redx2026"
+};
